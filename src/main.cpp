8ca305620bb0536e206be93c698e3c4ec61a2dbf
Emin Khateeb <emin@emin.ch>
2019-03-18 21:30:48 +0100

    improved decelaration

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ int throttlePin = A0;
 int throttle = 0;
 int speed = 1505;
 int realSpeed = 1505;
+int maxSpeed = 2000;
 mode currentMode = eco;
 
 void setup() {
@@ -16,27 +17,28 @@ void setup() {
 }
 
 void loop() {
+  switch (currentMode) {
+    case eco:
+      maxSpeed = 1800;
+      break;
+    case normal:
+      maxSpeed = 1800;
+      break;
+    case performance:
+      maxSpeed = 2000;
+      break;
+  }
+
   throttle = analogRead(throttlePin);
-  speed = map(throttle, 1023, 0, 1505, 2000);
+  speed = map(throttle, 1023, 0, 1505, maxSpeed);
 
   if (realSpeed < speed) {
     realSpeed++;
+    delay(20);
   }
 
   if (realSpeed > speed) {
     realSpeed--;
-  }
-
-  switch (currentMode) {
-    case eco:
-      delay(10);
-      break;
-    case normal:
-      delay(5);
-      break;
-    case performance:
-      delay(3);
-      break;
   }
 
   realSpeed = constrain(realSpeed, 1505, 2000);
